6b99d1ce13fd4bdb86b0342afc1f232f5579ae5b
Alexander <ADBrusob96@yandex.ru>
2020-07-11 20:23:47 +0300

    Правки

--- js/move.js
+++ js/move.js
@@ -24,15 +24,18 @@
         y: moveEvt.clientY
       };
 
-      var pinX = mainPin.offsetLeft - shift.x + window.map.MAIN_PIN_WIDTH / 2;
-      var pinY = mainPin.offsetTop - shift.y + window.map.MAIN_PIN_HEIGHT;
+      var mainPinX = mainPin.offsetLeft - shift.x + window.map.MAIN_PIN_WIDTH / 2;
+      var mainPinY = mainPin.offsetTop - shift.y + window.map.MAIN_PIN_HEIGHT;
 
-      if (pinX >= window.data.X_MIN && pinX <= window.data.xMax && pinY >= window.data.Y_MIN && pinY <= window.data.Y_MAX) {
+      if (mainPinY >= window.data.Y_MIN && mainPinY <= window.data.Y_MAX) {
         mainPin.style.top = (mainPin.offsetTop - shift.y) + 'px';
-        mainPin.style.left = (mainPin.offsetLeft - shift.x) + 'px';
-
-        window.map.addMainPinAddress(true);
       }
+
+      if (mainPinX >= window.data.X_MIN && mainPinX <= window.data.xMax) {
+        mainPin.style.left = (mainPin.offsetLeft - shift.x) + 'px';
+      }
+
+      window.map.addMainPinAddress(true);
     };
 
     var onMouseUp = function (upEvt) {
