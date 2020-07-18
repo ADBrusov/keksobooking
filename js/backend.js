5be467f299e59515ce0f3375722a91c809399020
Alexander <ADBrusob96@yandex.ru>
2020-07-18 16:15:50 +0300

    Добавляет функцию загрузки фото + рефакторинг

--- js/backend.js
+++ js/backend.js
@@ -8,46 +8,43 @@
     OK: 200
   };
 
+  var sendXhr = function (onSuccess, onError, method, url, data) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === StatusCode.OK) {
+        onSuccess(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = TIMEOUT_IN_MS;
+
+    xhr.open(method, url);
+    xhr.send(data);
+  };
+
+  var isDataLoad = function (onSuccess, onError) {
+    sendXhr(onSuccess, onError, 'GET', URL_DATA);
+  };
+
+  var isDataUpload = function (data, onSuccess, onError) {
+    sendXhr(onSuccess, onError, 'POST', URL, data);
+  };
+
+
   window.backend = {
-    isDataLoad: function (onSuccess, onError) {
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
-
-      xhr.addEventListener('load', function () {
-        if (xhr.status === StatusCode.OK) {
-          onSuccess(xhr.response);
-        } else {
-          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-        }
-      });
-
-      xhr.addEventListener('error', function () {
-        onError('Произошла ошибка соединения');
-      });
-
-      xhr.addEventListener('timeout', function () {
-        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-      });
-
-      xhr.timeout = TIMEOUT_IN_MS;
-
-      xhr.open('GET', URL_DATA);
-      xhr.send();
-    },
-    isDataUpload: function (data, onSuccess, onError) {
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
-
-      xhr.addEventListener('load', function () {
-        onSuccess();
-      });
-
-      xhr.addEventListener('error', function () {
-        onError();
-      });
-
-      xhr.open('POST', URL);
-      xhr.send(data);
-    }
+    isDataLoad: isDataLoad,
+    isDataUpload: isDataUpload
   };
 })();
---
+++ js/upload_img.js
@@ -0,0 +1,40 @@
+'use strict';
+
+(function () {
+  var avatarFileChooser = document.querySelector('#avatar');
+  var avatarPreview = document.querySelector('.ad-form-header__preview').querySelector('img');
+  var photoFileChooser = document.querySelector('#images');
+  var photoPreview = document.querySelector('.ad-form__photo').querySelector('img');
+
+  var uploadImg = function (fileChooser, preview) {
+    var FILE_TYPES = ['jpg', 'jpeg', 'png'];
+
+    var file = fileChooser.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        preview.src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+    }
+  };
+
+  var onAvatarChange = function () {
+    uploadImg(avatarFileChooser, avatarPreview);
+  };
+
+  var onPhotoChange = function () {
+    uploadImg(photoFileChooser, photoPreview);
+  };
+
+  avatarFileChooser.addEventListener('change', onAvatarChange);
+  photoFileChooser.addEventListener('change', onPhotoChange);
+})();
